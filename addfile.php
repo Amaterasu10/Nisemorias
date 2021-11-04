e2acbd84847afddfb41d14f1d8946b18fbffcc67
Hinrich <johnhinrichggalindo@gmail.com>
2021-11-04 23:51:09 +0800

    added script to disable submit button if there is not input file

--- addfile.php
+++ addfile.php
@@ -8,11 +8,23 @@
         <form action="upload.php" method="post" enctype="multipart/form-data">
             Select image to upload:
             <input type="file" name="fileToUpload" id="fileToUpload">
-            <input type="submit" value="Upload Image" name="submit">
+            <input type="submit" value="Upload Image" name="submit" disabled>
         </form>
     </Section>
     
 
 </body>
-
+ <script>
+    //  const body = on
+    const inputFile = document.querySelector("input[type='file']");
+    const submitButton = document.querySelector("input[type='submit']");
+    // alert(inputFile.value === "");
+    
+    inputFile.onchange = () => {
+        if(inputFile.value !== ""){
+            submitButton.removeAttribute('disabled');
+        }
+    }
+    
+ </script>
 </html>
