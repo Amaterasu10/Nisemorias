e4ad32b5534772e4325740cd9f1ae6fb557ad9f1
Hinrich <johnhinrichggalindo@gmail.com>
2021-11-08 17:02:23 +0800

    added register page and updated login to refer to it

--- register.php
+++ register.php
@@ -10,53 +10,21 @@
         <h3>You have to login first before you can access the gallery</h3>
         <div class="card" style="width: 32%;padding: 50px;>
             <form action="" method="post">
+                 <label for="name">Name:</label>
+                <input autocomplete="off" type="text" name="name">
+
                 <label for="username">Username:</label>
                 <input autocomplete="off" type="text" name="username">
                 
                 <label for="password">Password:</label>
                 <input autocomplete="off" type="text" name="password">     
                 
-                <button type="submit">Login</button>    
+                <button type="submit">Register</button>    
                 
-                <span class="centerer"><a href="register.php" class="btn btn--skew btn-default">Register</a></span>
+                <span class="centerer"><a href="login.php" class="btn btn--skew btn-default">Login</a></span>
 
             </form>
         </div>
-
-        <!-- <div class="container" id="container">
-	        <div class="form-container sign-up-container">
-                <form action="home.php">
-                    <h1>Create Account</h1>
-
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <button>Sign Up</button>
-                </form>
-            </div>
-            <div class="form-container sign-in-container">
-                <form action="home.php">
-                    <h1>Sign in</h1>
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <a href="#">Forgot your password?</a>
-                    <button>Sign In</button>
-                </form>
-            </div>
-            <div class="overlay-container">
-                <div class="overlay">
-                    <div class="overlay-panel overlay-left">
-                        <h1>Welcome Back!</h1>
-                        <p>To keep connected with us please login with your personal info</p>
-                        <button class="ghost" id="signIn">Sign In</button>
-                    </div>
-                    <div class="overlay-panel overlay-right">
-                        <h1>Hello, Friend!</h1>
-                        <p>Enter your personal details and start journey with us</p>
-                        <button class="ghost" id="signUp">Sign Up</button>
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </section>
 
     <?php
